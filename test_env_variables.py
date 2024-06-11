fddb10c2fc7fa1b7526db8e7150d94b43a672496
millie120701 <millieg120701@gmail.com>
2024-06-11 11:29:11 +0100

    comment

---
+++ test_env_variables.py
@@ -0,0 +1,7 @@
+import os
+
+print(os.environ.get('DB_NAME'))
+print(os.environ.get('DB_USER'))
+print(os.environ.get('DB_PASSWORD'))
+print(os.environ.get('DB_HOST'))
+print(os.environ.get('DB_PORT'))
