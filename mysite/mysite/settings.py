528d1c6153a74b19522f53818f4a7aa95c6d7b4f
millie120701 <millieg120701@gmail.com>
2024-06-18 12:20:45 +0100

    changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -66,18 +66,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ.get('DB_NAME', 'sudoku-db'),
-#         'USER': os.environ.get('DB_USER', 'user_admin'),
-#         'PASSWORD': os.environ.get('DB_PASSWORD', 'mypassword'),
-#         'HOST': os.environ.get('DB_HOST', 'localhost'),
-#         'PORT': os.environ.get('DB_PORT', '5432'),
-#     }
-# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'sudoku-db'),
+        'USER': os.environ.get('DB_USER', 'user_admin'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'mypassword'),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
