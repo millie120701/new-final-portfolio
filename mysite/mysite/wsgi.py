8dd4d26e20417b22f7bbe3e47d8cd69e0d3e0a7d
millie120701 <millieg120701@gmail.com>
2024-06-18 13:24:45 +0100

    procfile

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -4,13 +4,15 @@ WSGI config for mysite project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/stable/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite/mysite/settings.py')
+# Set the DJANGO_SETTINGS_MODULE environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
+# Create a WSGI application object
 application = get_wsgi_application()
