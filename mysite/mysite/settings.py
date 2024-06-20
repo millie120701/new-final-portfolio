e0d110ce44070ae3d0f21b7a1b0236a107190fba
millie120701 <millieg120701@gmail.com>
2024-06-21 00:11:56 +0100

    changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -2,8 +2,8 @@
 
 import os
 from pathlib import Path
-import dj_database_url
-import django_heroku
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
