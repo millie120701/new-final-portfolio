eb2435bb1eb48a4cc45a4f41826847ac9745ba5a
millie120701 <118777877+millie120701@users.noreply.github.com>
2024-06-20 23:35:42 +0100

    Update wsgi.py

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Set the DJANGO_SETTINGS_MODULE environment variable
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite/mysite/settings.py')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite/settings.py')
 
 # Create a WSGI application object
 application = get_wsgi_application()
