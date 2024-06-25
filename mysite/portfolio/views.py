da4a6f21094d299dc5e301bdd435b6d8711f9c0e
millie120701 <millieg120701@gmail.com>
2024-06-25 20:45:57 +0100

    folder changes

--- mysite/portfolio/views.py
+++ mysite/portfolio/views.py
@@ -4,17 +4,17 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'portfolio/index.html')
 
 def about(request):
-    return render(request, "about.html")
+    return render(request, "portfolio/about.html")
 
 
 def contact(request):
-    return render(request, "contact.html")
+    return render(request, "portfolio/contact.html")
 
 def projects(request):
-    return render(request, "projects.html")
+    return render(request, "portfolio/projects.html")
 
 def education(request):
-    return render(request, "education.html")
+    return render(request, "portfolio/education.html")
